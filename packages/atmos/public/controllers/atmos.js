cae282f3da2ac618fd1af323577bb199d3f2bcae
Thomas DE BONA <thomas.debona@gmail.com>
2014-06-19 00:04:08 +0200

    Display the cities’ atmo indexes

--- packages/atmos/public/controllers/atmos.js
+++ packages/atmos/public/controllers/atmos.js
@@ -1,23 +1,16 @@
 'use strict';
 
-angular.module('mean.atmos').controller('AtmosController', ['$scope', 'Global', 'Atmos',
-    function($scope, Global, Atmos) {
+angular.module('mean.atmos').controller('AtmosController', ['$scope', 'Global', 'Cities',
+    function($scope, Global, Cities) {
         $scope.global = Global;
         $scope.package = {
             name: 'atmos'
         };
 
-        $scope.cities = [
-            {'name': 'paris'},
-            {'name': 'marseille'},
-            {'name': 'lyon'},
-            {'name': 'toulouse'},
-            {'name': 'nice'},
-            {'name': 'nantes'},
-            {'name': 'strasbourg'},
-            {'name': 'montpellier'},
-            {'name': 'bordeaux'},
-            {'name': 'lille'},
-        ];
+        $scope.find = function() {
+            Cities.query(function(cities) {
+                $scope.cities = cities;
+            });
+        };
     }
 ]);
